6421caa25ea523320e649da0a20087c494527315
TMiyazono <miata3830@icloud.com>
2024-11-24 22:29:00 +0900

    20241124

--- work/js/app.js
+++ work/js/app.js
@@ -1,17 +1,20 @@
-let url = "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/{$cityCode}.json";
- 
+let url = "https://www.jma.go.jp/bosai/forecast/data/forecast/{$cityCode}.json";
+
 fetch(url)
-   .then(function(response) {
-       return response.json();
-   })
-   .then(function(weather) {
-       console.log(weather);
-       // 画面に書き出す
-       document.getElementById("publishingOffice").lastElementChild.textContent = weather.publishingOffice;
-       document.getElementById("reportDatetime").lastElementChild.textContent = weather.reportDatetime;
-       document.getElementById("targetArea").lastElementChild.textContent = weather.targetArea;
-       document.getElementById("todayHighTemperature").lastElementChild.textContent = weather.todayHighTemperature;
-       document.getElementById("todayLowTemperature").lastElementChild.textContent = weather.todayLowTemperature;
-       document.getElementById("today").lastElementChild.textContent = weather.today;
-       document.getElementById("tomorrow").lastElementChild.textContent = weather.tomorrow;
-   });
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(weather) {
+        console.log(weather);
+        // 特定の地域(今回は東京)だけ選択して変数に詰め直す
+        let area = weather[0].timeSeries[0].areas[0];
+        console.log(area); 
+        // 発表者と報告日時の情報を画面に書き出す
+        document.getElementById("publishingOffice").lastElementChild.textContent = weather[0].publishingOffice;
+        document.getElementById("reportDatetime").lastElementChild.textContent = weather[0].reportDatetime;
+        // 特定地域の情報を画面に書き出す
+        document.getElementById("targetArea").lastElementChild.textContent = area.area.name;
+        document.getElementById("today").lastElementChild.textContent = area.weathers[0];
+        document.getElementById("tomorrow").lastElementChild.textContent = area.weathers[1];
+        document.getElementById("dayAfterTomorrow").lastElementChild.textContent = area.weathers[2];
+    });
